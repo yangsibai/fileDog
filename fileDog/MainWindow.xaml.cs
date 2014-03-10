b21f1ce5345f13be4658452357b187f6f7f47a42
osrpt <yangsibai@gmail.com>
2014-03-11 01:11:05 +0800

    ui layer

---
+++ fileDog/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace fileDog
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private TaskConfig config = new TaskConfig()
+        {
+            StartURL = "http://www.ireadhome.com"
+        };
+
+        public MainWindow()
+        {
+            this.DataContext = config;
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击开始按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+//            TaskConfig config = (TaskConfig) this.FindResource("Config");
+            MessageBox.Show("start url" + config.StartURL);
+        }
+
+        /// <summary>
+        /// 点击结束按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ fileDog/TaskConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using fileDog.Annotations;
+
+namespace fileDog
+{
+    /// <summary>
+    /// 任务设置
+    /// </summary>
+    public class TaskConfig
+    {
+        /// <summary>
+        /// 开始地址
+        /// </summary>
+        public String StartURL { get; set; }
+
+        /// <summary>
+        /// min size
+        /// </summary>
+        public int FileMinSize { get; set; }
+
+        /// <summary>
+        /// max size
+        /// </summary>
+        public int FileMaxSize { get; set; }
+
+        /// <summary>
+        /// 重命名文件
+        /// </summary>
+        public bool RenameFile { get; set; }
+
+        /// <summary>
+        /// 启用代理
+        /// </summary>
+        public bool EnableProxy { get; set; }
+    }
+}
